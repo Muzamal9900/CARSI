7f28d74410780fc1cb488f477eb602b93a4df34b
Rana Muzamil <mmlrana00@gmail.com>
2026-03-31 01:12:57 +0500

    chore: commit src/components/layout/PageTransition.tsx

--- src/components/layout/PageTransition.tsx
+++ src/components/layout/PageTransition.tsx
@@ -17,6 +17,7 @@ export function PageTransition({ children }: PageTransitionProps) {
     <AnimatePresence mode="wait">
       <motion.div
         key={pathname}
+        className="min-h-0 w-full flex-1"
         initial={{ opacity: 0, y: 8 }}
         animate={{ opacity: 1, y: 0 }}
         exit={{ opacity: 0, y: -8 }}
